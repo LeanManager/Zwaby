1455f7c3d652c9394bb7a91ec50124836b691184
LeanManager <zwabyapp@gmail.com>
2017-10-26 20:26:56 -0400

    Stripe successful integration

--- ZwabyWebServices/Controllers/PaymentController.cs
+++ ZwabyWebServices/Controllers/PaymentController.cs
@@ -20,22 +20,33 @@ namespace ZwabyWebServices.Controllers
             // Because this is just an example, you can hard code the Stripe Secret and Publishable Key here
             // This WebApi will then need to be hosted on a server, e.g. Azure
 
-            var customers = new StripeCustomerService();
-            var charges = new StripeChargeService();
+            //var customers = new StripeCustomerService();
+            //var charges = new StripeChargeService();
+            //var customer = customers.Create(new StripeCustomerCreateOptions
+            //{
+            //    Email = payment.Email,
+            //    SourceToken = payment.Token
+            //});
+            //var charge = charges.Create(new StripeChargeCreateOptions
+            //{
+            //    Amount = payment.Amount, // in cents
+            //    Description = "Booking price",
+            //    Currency = "usd",
+            //    CustomerId = customer.Id
+            //});
 
-            var customer = customers.Create(new StripeCustomerCreateOptions
-            {
-                Email = payment.Email,
-                SourceToken = payment.Token
-            });
+            StripeConfiguration.SetApiKey("sk_test_t0PxK1ifMhrPBI8LXM8zk6eD");
 
-            var charge = charges.Create(new StripeChargeCreateOptions
+            var chargeOptions = new StripeChargeCreateOptions()
             {
-                Amount = payment.Amount, // in cents
-                Description = "Booking price",
+                Amount = payment.Amount,
                 Currency = "usd",
-                CustomerId = customer.Id
-            });
+                Description = "Zwaby Charge",
+                SourceTokenOrExistingSourceId = payment.Token
+            };
+
+            var chargeService = new StripeChargeService();
+            StripeCharge charge = chargeService.Create(chargeOptions);
 
             // Once the payment is made, return the result here
             // Please note that this way of doing things, is just because this is just a proof of concept app for you.
